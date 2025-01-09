6b5976b6cf9157f4430a5604fc23d9cbade4f7b7
taimek <waszkitymek@gmail.com>
2025-01-09 17:21:41 +0000

    created friendship model and profile

--- MyNostalgia/~/.venv/mysite/polls/models.py
+++ MyNostalgia/~/.venv/mysite/polls/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    bio = models.TextField(blank=True, null=True)
+    custom_html = models.TextField(blank=True, null=True)  # Użytkownik może tu wpisać własny kod HTML
+    custom_css = models.TextField(blank=True, null=True)  # Użytkownik może tu wpisać własny kod CSS
+
+    def __str__(self):
+        return self.user.username
+
+class Friendship(models.Model):
+    from_user = models.ForeignKey(User, related_name='sent_requests', on_delete=models.CASCADE)
+    to_user = models.ForeignKey(User, related_name='received_requests', on_delete=models.CASCADE)
+    accepted = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.from_user} -> {self.to_user} ({'Accepted' if self.accepted else 'Pending'})"
